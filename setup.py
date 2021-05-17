038bb3392afe2075126a60f78418ffa2b02be6db
Kavi Gupta <kavi@berkeley.edu>
2021-05-17 01:36:40 -0700

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="electiondata",
-    version="1.4.0",
+    version="1.5.0",
     author="Kavi Gupta",
     author_email="electiondata@kavigupta.org",
     description="Set of APIs and scripts for normalizing election data.",
